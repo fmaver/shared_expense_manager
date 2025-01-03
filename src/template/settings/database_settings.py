1b6488a87ec29463172d1710892c8101d80fd6d2
fmaver <fmaver@frba.utn.edu.ar>
2025-01-03 18:59:31 -0300

    Add variable to determine env

--- src/template/settings/database_settings.py
+++ src/template/settings/database_settings.py
@@ -7,7 +7,16 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class DatabaseSettings(BaseSettings):
     """Database configuration settings."""
 
-    url: str = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@db:5432/expense_manager")
+    database_env: str = os.getenv("DATABASE_ENV", "QA")
+    url: str
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        if self.database_env == "QA":
+            self.url = os.getenv("QA_DATABASE_URL", "postgresql://postgres:postgres@db:5432/expense_manager")
+        else:
+            self.url = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@db:5432/expense_manager")
+
     echo: bool = False
 
     model_config = SettingsConfigDict(env_prefix="DATABASE_")
